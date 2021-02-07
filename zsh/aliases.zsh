6c6ed616f32a227bb0cc2b5c0532cb80f885d92a
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2021-02-07 00:08:34 +0000

    Don't expect docker to be on when zsh loads

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -73,8 +73,8 @@ alias lock-pl='fligo lock -e playground futurelearn --create'
 alias cow="fortune | cowsay"
 
 # Docker
-alias prune-af="docker system prune -a -f" # clean everything
-alias docker-rm-all="docker rm -f $(docker images -aq)" # removes all images
+# alias prune-af="docker system prune -a -f" # clean everything
+# alias docker-rm-all="docker rm -f $(docker images -aq)" # removes all images
 
 # Vim
 # Install vim plugins via vundle
