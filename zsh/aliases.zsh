e405e1c8e008037523b89520e15b58b90220f7c3
Elena Tanasoiu <elena.tanasoiu@gmail.com>
2021-02-15 11:22:46 +0000

    Add alias for GoLand

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -68,6 +68,9 @@ alias minedns='open -a rubymine ~/projects/dns'
 alias minesq='open -a rubymine ~/projects/squiddy'
 alias minet='open -a rubymine ~/projects/terraform'
 
+# GoLand
+alias minego='open -a goland ~/projects/fligo'
+
 # FL shortcuts
 alias ffs='bundle install && bundle exec rake db:migrate && bundle exec rake db:test:prepare && yarn'
 alias lock-pl='fligo lock -e playground futurelearn --create'
