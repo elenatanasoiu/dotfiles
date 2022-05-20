ddfd8388f63496e95fda37f33bc5083e975cc046
Elena Tanasoiu <elenatanasoiu@github.com>
2022-05-20 18:25:20 +0100

    Skip oh-my-matrix plugin

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,2 +1,2 @@
 # Plugins
-plugins=(ssh-agent git history-substring-search colored-man-pages docker-compose oh-my-matrix emoji)
+plugins=(ssh-agent git history-substring-search colored-man-pages docker-compose emoji)
